8e09c789bd446a196542b9b0cbfe4d5b903c3a8d
Norbert <8168846+sz3lbi@users.noreply.github.com>
2021-06-24 23:39:01 +0200

    limit available languages to those which have locale

--- sm_project/settings.py
+++ sm_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config, Csv
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,6 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGES = [
+    ("pl", _("Polish")),
+    ("en", _("English")),
+]
 
 LANGUAGE_CODE = "pl"
 
